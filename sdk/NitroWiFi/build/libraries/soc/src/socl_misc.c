738cdd394c969c83208b64ac105109b0f86ae1d1
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-14 17:57:19 -0500

    Fix compilation

--- sdk/NitroWiFi/build/libraries/soc/src/socl_misc.c
+++ sdk/NitroWiFi/build/libraries/soc/src/socl_misc.c
@@ -32,7 +32,21 @@ struct errortable
 
 static struct errortable    errtable[] =
 {
-#include "errtable.h"
+{ SOC_ENOMEM, "ENOMEM" },
+{ SOC_EMFILE, "EMFILE" },
+{ SOC_ECONNRESET, "ECONNRESET" },
+{ SOC_ECANCELED, "ECANCELED" },
+{ SOC_EALREADY, "EALREADY" },
+{ SOC_ENOTCONN, "ENOTCONN" },
+{ SOC_ENETRESET, "ENETRESET" },
+{ SOC_EINVAL, "EINVAL" },
+{ SOC_ENOBUFS, "ENOBUFS" },
+{ SOC_ETIMEDOUT, "ETIMEDOUT" },
+{ SOC_EWOULDBLOCK, "EWOULDBLOCK" },
+{ SOC_EAGAIN, "EAGAIN" },
+{ SOC_EINPROGRESS, "EINPROGRESS" },
+{ SOC_EISCONN, "EISCONN" },
+{ SOC_EMSGSIZE, "EMSGSIZE" },
 };
 
 /*---------------------------------------------------------------------------*
